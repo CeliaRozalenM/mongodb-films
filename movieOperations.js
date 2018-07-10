010bb86086f24ff6f09e74fe454c9db8e672f047
CeliaR <celia.rozalen@gmail.com>
2018-07-10 21:39:55 +0200

    Push unestaged files

--- movieOperations.js
+++ movieOperations.js
@@ -1,5 +1,5 @@
-var Director= require('./movies');
-var Movie= require('./movies');
+var movies= require('./movies');
+var directors= require('./directors');
 
 function createMovie(){
 
@@ -13,44 +13,44 @@ function createMovie(){
     }
 
     function getDirectorName(){
-        return  listNames[getRandomArbitrary(listNames.length, 0)] + " " + listSurnames[getRandomArbitrary(listSurnames.length, 0)];
+        return  listNames[getRandomArbitrary(listNames.length-1, 0)] + " " + listSurnames[getRandomArbitrary(listSurnames.length-1, 0)];
     }
  
     function getMovieTitle(){
-        return  listMovieTitle_1[getRandomArbitrary(listMovieTitle_1.length, 0)] + " " + listMovieTitle_2[getRandomArbitrary(listMovieTitle_2.length, 0)];
+        return  listMovieTitle_1[getRandomArbitrary(listMovieTitle_1.length-1, 0)] + " " + listMovieTitle_2[getRandomArbitrary(listMovieTitle_2.length-1, 0)];
     }
     
     function getRandomYear(){
         return  getRandomArbitrary(1900, 2000);
     }
 
-    //Create an object with the data that we want to save. Same name of require variable!
-
-    var director = new Director();
-    var movie = new Movie();
+    //Create an object with the data that we want to save. Same name of "require variable"!
+    var director = new directors.Directors();
 
     //Call the variable, not the object
     director.name = getDirectorName();
-    director.birth= getRandomYear();
-
+    director.birth = getRandomYear();
+    console.log(director);
+    
     director.save(function(err){
         if(err){
-            console.log("There is an "+ err.message)
+            console.log("There is an "+ err.message);
         }else{
-            console.log("Director inserted correctly")
+            console.log("Director inserted correctly");
+            var movie = new movies.Movies();
+            movie.title = getMovieTitle();
+            movie.year = getRandomYear();
+            //Reference id director
+            movie.director = director.id; 
+            movie.save(function(err){
+                if(err){
+                    console.log("There is an "+ err.message)
+                }else{
+                    console.log("Movie inserted correctly")
+                }
+            });
         }
-    })
-
-    movie.name = getMovieTitle();
-    movie.year= getRandomYear();
-
-    movie.save(function(err){
-        if(err){
-            console.log("There is an "+ err.message)
-        }else{
-            console.log("Movie inserted correctly")
-        }
-    })    
+    })   
 }
 
 module.exports = {
--- movies.js
+++ movies.js
@@ -3,19 +3,16 @@ var mongoose = require('mongoose');
 //Schemas
 var Schema = mongoose.Schema;
 var moviesSchema = new Schema({
-    name: {type: String, required: true, unique: true },
-    year: Number
-});
-
-var directorsSchema = new Schema({
-    name: {type: String, required: true, unique: true },
-    birth: Number
+    title: {type: String, required: true},
+    year: Number,
+    //Reference to Directors collection
+    director: {type: mongoose.Schema.Types.ObjectId, ref:'Directors'}
 });
 
 //Create model to export schema. The parameters are "Model Name - (Collection name)" and "Schema Name"
 var Movies = mongoose.model('Movies', moviesSchema);
-var Directors = mongoose.model('Directors', directorsSchema);
 
 //Export model
-module.exports = Movies;
-module.exports = Directors;
+module.exports = {
+    Movies
+}
